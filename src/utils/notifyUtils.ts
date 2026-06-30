c49a92d6806c2b980b384c5cb1463cac29b6de9c
SAM <sam@gmail.com>
2026-06-30 16:37:29 +0800

    feat:歷程記錄增加狀態分類、錯誤提示改成notify

--- src/utils/notifyUtils.ts
+++ src/utils/notifyUtils.ts
@@ -3,12 +3,17 @@ import { Notify } from 'quasar';
 let dismissPreviousNotify: (() => void) | null = null;
 
 export const showNotify = (message: string, type: 'positive' | 'negative') => {
-  if (dismissPreviousNotify) dismissPreviousNotify();
+  // 如果有舊通知，先關閉它
+  if (dismissPreviousNotify) {
+    dismissPreviousNotify();
+  }
+
+  // 顯示新通知，並存下關閉函式
   dismissPreviousNotify = Notify.create({
     type: type,
     message: message,
     position: 'top',
     timeout: 3000,
-    group: false
+    group: false, // 確保不群組化，方便我們精確控制
   });
 };
